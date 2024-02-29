6fa6de84d450a48e3b36a028ebb52c57a46f80fa
TARUN CHAND I <51223144+Tarun-Chand-Illapu@users.noreply.github.com>
2024-02-29 17:30:11 -0500

    webserver1.sh

---
+++ webserver1.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+apt update
+apt install -y apache2
+
+apt install -y awscli
+
+cat <<EOF > /var/www/html/index.html
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Terraform Project</title>
+  <style>
+    /* Add animation and styling for the text */
+    @keyframes colorChange {
+      0% { color: red; }
+      50% { color: green; }
+      100% { color: blue; }
+    }
+    h1 {
+      animation: colorChange 2s infinite;
+    }
+  </style>
+</head>
+<body>
+  <h1>Welcome to Server 1</h1>
+  
+</body>
+</html>
+EOF
+
+# Start Apache and enable it on boot
+systemctl start apache2
+systemctl enable apache2
